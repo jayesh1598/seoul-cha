3e5f54e8094fb19c1814eb05ed0990358d4ba3ab
jayesh suthar <39787261+jayesh1598@users.noreply.github.com>
2025-08-30 15:24:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
   export default defineConfig({
     plugins: [react()],
+     build: { outDir: 'dist' } 
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
